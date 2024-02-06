79a0c75d0d9a69e94e5cc519d012f3ab7b984c09
s3bxst1 <s3bxs@outlook.com>
2024-02-05 22:48:19 -0500

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,33 @@
+import java.time.Year;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String ID;
+    private String title;
+    private int YOB;
+
+    public Person(String firstName, String lastName, String ID, String title, int YOB) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ID = ID;
+        this.title = title;
+        this.YOB = YOB;
+    }
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String formalName() {
+        return title + " " + fullName();
+    }
+
+    public int getAge() {
+        return getAge(Year.now().getValue());
+    }
+
+    public int getAge(int year) {
+        return year - YOB;
+    }
+}
